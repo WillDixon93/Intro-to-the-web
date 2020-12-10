1ced2cd44d5b1acbfa4b57cf0ef5cf85d47d2f4b
Will Dixon <dixon.will1@gmail.com>
2020-12-10 13:53:52 +0000

    sinatra erb

--- app.rb
+++ app.rb
@@ -1,9 +1,15 @@
 require 'sinatra'
 
+set :session_secret, 'super secret'
+
 get '/' do
-    "Hello!"
+    "Hello! my name is billy"
 end
 
 get '/secret' do
     "This is a secret page"
+end
+
+get '/cat' do
+    erb(:index)
 end
